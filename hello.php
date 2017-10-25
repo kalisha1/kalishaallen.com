e9bc8b06342050ec9956b5d744346d6962087c3d
kalisha <allen.kalisha@gmail.com>
2017-10-25 16:02:26 -0400

    inital commit

---
+++ hello.php
@@ -0,0 +1,56 @@
+<?php
+// $num_one = 1;
+// $num_two = 2;
+// $num_three =3;
+//My first php script//
+// var_dump ($num_one);
+// var_dump (1);
+// var_dump ("1");
+// var_dump ($num_one + $num_two -$num_three);
+//this is the second command line//
+//  $distance_home = 1.2;
+//  $distance_work = 2.5;
+//  var_dump ($distance_work - $distance_home + $num_three);
+
+
+//  $a=5;
+//  $b=10;
+//  var_dump($a *$b);
+//  var_dump($a/$b);
+//  $a= $a +1;
+//  var_dump($a);
+//  $a++;
+//  $a += 5;
+
+//number in pounds we want to convert to kilograms 
+$pounds=140;
+//floating point value for the pund to kilogram converstion
+$lbs_to_kg =0.45392;
+//use the variables above to calcuate pounds multioles by the kilogram conversion 
+$kilograms = $pounds * $lbs_to_kg;
+//display the pounds to kilograms 
+echo "weight:";
+echo $pounds;
+echo " lbs = ";
+echo $kilograms;
+echo "kg";
+//number in miles we want to convert to kilometers
+$miles=2.5;
+//floating point value for the mile to kilometer converstion 
+$miles_to_km =1.60934;
+//use the variable above to caluate miles muiltiples by kilometer converstion
+$kilometers = $miles * $miles_to_km;
+//display results to screen
+echo "Distance";
+echo $miles;
+echo "miles = ";
+echo $kilometer;
+echo "km";
+
+
+
+ ?>
+
+
+
+
